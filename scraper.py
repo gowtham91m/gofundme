c946ce6c17c0e18abcf26ea121222b79a8c5ae20
GOWTHAM MALLIKARJUNA <gowtham.91m@gmail.com>
2018-08-30 16:26:36 -0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,7 +2,6 @@
 
 #author: Gowtham Mallikarjuna
 
-
 from bs4 import BeautifulSoup as bs
 import requests
 from IPython.display import display, clear_output
@@ -15,14 +14,44 @@ from collections import OrderedDict
 from datetime import datetime
 from itertools import accumulate
 import os
+import logging
+import subprocess
+import getpass
 
 class web_scraper:
     def __init__(self):
         self.url = 'https://www.gofundme.com/discover'
         self.campaign_columns = ['category','page','title','href','location','start_date','goal','raised',
                                  'text','likes','shares','photos','donation_count','duration',
-                                 'recent_donation_time','goal_reaeched_time','script_run_date']
+                                 'recent_donation_time','goal_reaeched_time','script_run_time']
 
+    def read_data(self,path):
+      os.chdir(path)
+      if 'campaigns.csv' not in os.listdir():
+            campaign_data = pd.DataFrame(OrderedDict({i:[] for i in self.campaign_columns}))
+            campaign_data.to_csv('campaigns.csv',index=False)
+      else:
+            campaign_data = pd.read_csv('campaigns.csv')
+      return campaign_data
+      
+    def git_clone(self):
+      
+      if 'gofindme' not in os.listdir(os.getcwd()):
+        os.chdir('/content')
+        username = input('username:')
+        password = getpass.getpass('Password:')
+        gt = 'https://'+username+':'+password+'@github.com/gowtham91m/gofundme.git'
+        subprocess.Popen(['git', 'clone', str(gt)])
+        os.chdir('gofundme/data/')
+      else:
+        os.chdir('data')
+        subprocess(['git','pull'])
+        
+    def git_push(self):
+      subprocess.Popen(['git','add','campaigns.csv'])
+      subprocess.Popen(['git','commit','-m','commit'])
+      subprocess.Popen(['git','push','-u','origin','master'])
+        
     def get_categories(self):
         soup = requests.get(self.url)
         soup = bs(soup.text,'html.parser')
@@ -50,7 +79,6 @@ class web_scraper:
         try: start_date = soup.findAll(class_='created-date')[0].text[8:]
         except Exception as e:
           print('error getting start date:',e)
-          #logger.error('error getting start date for category'+category+', url'+url+': e')
           start_date = e
         try: 
           donation = soup.findAll(class_='campaign-status text-small')[0].text.strip()
@@ -63,7 +91,6 @@ class web_scraper:
           goal = re.findall('\$\d+.*',funds)[1].split(' ')[0]
           
           print('\n',url[25:],raised,'/',goal,'-',donation_count,' ',end='')
-          #logger.info(url[25:],raised, '/', donation_count, ' ')
           if int(re.sub('[^\d]','',raised)) >= int(re.sub('[^\d]','',goal)):
             min_completion_time = self.get_min_goal_time(url,goal)
           else:
@@ -74,7 +101,7 @@ class web_scraper:
         return OrderedDict({'category':category,'page':page,'title':title, 'href':url, 'location':location,'start_date':start_date ,'goal':goal 
                             ,'raised':raised, 'text':text ,'likes':likes,'shares':shares, 'photos':photos,  'donation_count':donation_count
                             ,'duration':duration, 'recent_donation_time':recent_donation_time , 'goal_reaeched_time':min_completion_time
-                            ,'script_run_date':datetime.today().strftime("%Y-%m-%d")})
+                            ,'script_run_time':datetime.today().strftime("%Y-%m-%d")})
 
     def get_min_goal_time(self,href,goal):
         goal=int(re.sub('[^\d]','',goal))
@@ -99,26 +126,17 @@ class web_scraper:
         for i in range(len(d)):
           if d[i]>goal:
             return time_gap[-i-1]
-    
-    def read_data(self, path = os.getcwd()):
-      os.chdir(path)
-      if 'campaigns.csv' not in os.listdir():
-            campaign_data = pd.DataFrame(OrderedDict({i:[] for i in self.campaign_columns}))
-            campaign_data.to_csv('campaigns.csv',index=False)
-      else:
-            campaign_data = pd.read_csv('campaigns.csv')
-      return campaign_data
-      
 
-    def get_campaigns(self,categories = 'all'):
+    def get_campaigns(self,categories = 'all',path=os.getcwd(),skip = ['']):
         start_time = time()
         df = pd.DataFrame({})
         if categories == 'all':
           categories = self.get_categories()
-        campaigns =self.read_data()
+        campaigns =self.read_data(path)
         for i in categories:
+          if i in skip:
+            continue
           print(i,end='  ')
-          #logger.info(i)
           i='-'.join(i.split(' '))
           i = 'animal' if i == 'Animals' else i
           url = 'https://www.gofundme.com/discover/'+i+'-fundraiser'
@@ -131,7 +149,6 @@ class web_scraper:
             
           while True:
             print('\n',page)
-            #logger.info(page)
             url = 'https://www.gofundme.com/mvc.php?route=categorypages/load_more&page='+str(page)+'&term=&cid='+cid
             soup = requests.get(url)
             soup = bs(soup.text, 'html.parser')
@@ -144,16 +161,18 @@ class web_scraper:
             if len(details)>0:   
               df = pd.DataFrame(details)[self.campaign_columns]
               df.to_csv('campaigns.csv',index=False,header=False,mode='a')
-            
             #if (page%50==0):break
             page+=1        
                 
           print('\n')
         #clear_output()
         print('campaigns scrape time', time()-start_time)
-        #logger('campaigns scrape time', time()-start_time)
+
 
 if __name__ == '__main__':
     path = 'G:\\My Drive\\codelab\\gofundme'
     #path = os.getcwd()
-    web_scraper().get_campaigns(['Medical'])
+    scraper = web_scraper()
+    #scraper.git_clone()
+    #scraper.get_campaigns() 
+    #scraper.git_push()
